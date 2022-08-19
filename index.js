9c0a24f5c92912f3e2db30ef119b08602f919377
Anna Baraulina <a.baraulina@yandex.ru>
2022-08-19 13:58:12 +0300

    likes and modal

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const likeButtons = document.querySelectorAll('.like-icon');
+const openModalBtn = document.querySelector('.ready-button');
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-modal');
 
 
 likeButtons.forEach((elem) => {
     elem.addEventListener('click', () => {
         elem.classList.toggle('like-icon_active');
     })
+})
+
+
+
+
+openModalBtn.addEventListener('click', () => {
+    popup.classList.add('popup_open');
+    
+} );
+
+closeBtn.addEventListener('click', () => {
+    popup.classList.remove('popup_open');
 })
